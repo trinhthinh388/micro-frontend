eb027d1941612b812b0a19ff38ddbe15707acd25
thinh.trinh <thinhtd@wolffungame.com>
2022-07-09 11:19:03 +0700

    feat: add react app

---
+++ packages/react-side-menu/src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Menu from './Menu';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <Menu />
+  </React.StrictMode>
+);
